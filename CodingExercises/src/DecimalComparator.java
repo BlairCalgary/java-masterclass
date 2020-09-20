600b93faeb24901299e3435350aeb887273eca1f
BlairCalgary <59458311+BlairCalgary@users.noreply.github.com>
2020-09-20 13:08:32 -0600

    more coding exercises

--- CodingExercises/src/DecimalComparator.java
+++ CodingExercises/src/DecimalComparator.java
@@ -1,5 +1,12 @@
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces (double param1, double param2) {
+        int myFirstCheck = (int)(param1 * 1000);
+        int mySecondCheck = (int)(param2 * 1000);
 
+        if (myFirstCheck==mySecondCheck) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- CodingExercises/src/Main.java
+++ CodingExercises/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
 //        MegaBytesConverter.printMegaBytesAndKiloBytes(-5000);
 //        boolean wakeUp = BarkingDog.shouldWakeUp(true, -1);
 //        System.out.println(wakeUp);
-        boolean leapYear = LeapYear.isLeapYear(1924);
-        System.out.println(leapYear);
+//        boolean leapYear = LeapYear.isLeapYear(1924);
+//        System.out.println(leapYear);
+//        boolean areEqual = DecimalComparator.areEqualByThreeDecimalPlaces(-3.123,3.123);
+//        System.out.println(areEqual);
+//        boolean areEqual = EqualSumChecker.hasEqualSum(1,-1,0);
+//        System.out.println(areEqual);
+        boolean hasTeen = TeenNumberChecker.hasTeen(22,23,34);
+        System.out.println(hasTeen);
+        boolean isTeen = TeenNumberChecker.isTeen(13);
+        System.out.println(isTeen);
     }
 }
